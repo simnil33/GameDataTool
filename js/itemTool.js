2d57630d133d1af26949734bddd376db59b46002
Johannes Nilsson <johannes@nilsson.im>
2021-03-14 15:47:26 +0100

    Clear files

--- js/itemTool.js
+++ js/itemTool.js
@@ -1,9 +1,4 @@
 // loads the map tool when button is clicked
 function loadItemTool(){
-	intro.innerHTML = '<h1>Item Tool</h1><p>This is the item tool!</p>';
 
-	toolBox.innerHTML = '<p>This item tool page just has a paragraph right now, no tool.</p>';
-
-	secondaryMenu.style.display = 'inline-block';
-	secondaryMenu.innerHTML = '<button class="menuItem" id="buildGrid" onclick="buildGrid()">Random Button</button><button class="menuItem" id="clearGrid" onclick="clearGrid()"> Random Button</button><button class="menuItem" id="randomGrid" onclick="randomGrid()">Random Button</button>';
 }
--- js/questTool.js
+++ js/questTool.js
@@ -1,9 +1,4 @@
 // loads the map tool when button is clicked
 function loadQuestTool(){
-	intro.innerHTML = '<h1>Quest Tool</h1><p>This is the quest tool!</p>';
 
-	toolBox.innerHTML = '<p>This quest tool page just has a paragraph right now, no tool.</p>';
-
-	secondaryMenu.style.display = 'none';
-	secondaryMenu.innerHTML = '';
 }
